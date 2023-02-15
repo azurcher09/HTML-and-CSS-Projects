e8b6a3d2b3557914977fb35def7e46fbc343d68f
azurcher09 <124114026+azurcher09@users.noreply.github.com>
2023-02-14 18:00:50 -0700

    Committing HTML and JavaScript folder
    I am committing my Basic_HTML_and_JavaScript_1 folder containing my Basic HTML and JavaScript assignments and challenges.

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("This is a test alert box!");
+
+function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
